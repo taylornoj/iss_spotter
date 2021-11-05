9e439c04e7162c896d425e3f12bf956a4a9a3a8b
Taylor Noj <taylornoj@gmail.com>
2021-11-05 01:18:14 +0000

    linted

--- iss.js
+++ iss.js
@@ -8,17 +8,25 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
- const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
+    // inside the request callback ...
+  // error can be set if invalid domain, user is offline, etc.
     if (error) {
       callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
       return;
     }
 
     const ip = JSON.parse(body).ip;
     callback(null, ip);
     
-  })
-}
+  });
+};
 
 module.exports = { fetchMyIP };
