5e04ed82109e107ab3c303c46b6e189f93817ec7
Taylor Noj <taylornoj@gmail.com>
2021-11-05 11:19:28 +0000

    linted

--- index2.js
+++ index2.js
@@ -1,4 +1,18 @@
-const { fetchMyIP } = require("./iss_promised");
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(body => console.log(body)); 
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
--- iss_promised.js
+++ iss_promised.js
@@ -1,7 +1,38 @@
 const request = require('request-promise-native');
 
-const fetchMyIP = function() {
+const fetchMyIP = () => {
   return request('https://api.ipify.org?format=json');
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+// const printPassTimes = function(passTimes) {
+//   for (const pass of passTimes) {
+//     const datetime = new Date(0);
+//     datetime.setUTCSeconds(pass.risetime);
+//     const duration = pass.duration;
+//     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+//   }
+// };
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+
+module.exports = { nextISSTimesForMyLocation };
